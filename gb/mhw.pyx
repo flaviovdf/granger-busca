434794a949b1b413b8346c85676bfe01b8dea671
Flavio Figueiredo <flaviovdf@dcc.ufmg.br>
2017-12-23 19:49:01 -0200

    Still working on he walker

--- gb/mhw.pyx
+++ gb/mhw.pyx
@@ -61,13 +61,14 @@ cdef inline int sample_background(double prob_background) nogil:
     return rand() < prob_background
 
 
-cdef inline double busca_rate(double alpha_ba, double tp, double tpp) nogil:
+cdef inline double busca_rate(double alpha_ba, double tp, double tpp,
+                              double beta_proc_b) nogil:
     '''Computes the busca rate given alpha and dt'''
     cdef double rate = alpha_ba / (beta_proc_b/E + tp - tpp)
     return rate
 
 
-cdef inline void busca_probability(int proc_b, proc_a,
+cdef inline void busca_probability(int proc_b, int proc_a,
                                    map[int, map[int, int]] &Alpha_ab,
                                    int[::1] sum_b, double alpha_prior,
                                    double[::1] beta_rates,
@@ -75,47 +76,38 @@ cdef inline void busca_probability(int proc_b, proc_a,
                                    double *a_ba, double *rate) nogil:
 
     a_ba[0] = dirmulti_posterior(Alpha_ab, sum_b, proc_b, proc_a, alpha_prior)
-    cdef int n_proc = <int>all_timestamps.size()
-    rate[0] = busca_rate(i, proc_a, proc_b, all_timestamps, a_ba[0],
-                         beta_rates[proc_b], lower_stamps)
+    rate[0] = busca_rate(a_ba[0], tp, tpp, beta_rates[proc_b])
 
 
-cdef inline int metropolis_walk_step(int proc_a, int i, int curr_influencer_b,
-                                     double prev_back_t,
-                                     int[::1] num_background,
-                                     map[int, vector[double]] &all_timestamps,
-                                     vector[int] &curr_state_proc_a,
+cdef inline int metropolis_walk_step(int proc_a, int curr_influencer_b,
+                                     double t, double[::1] last_t,
+                                     double dt_background,
                                      map[int, map[int, int]] &Alpha_ab,
                                      int[::1] sum_b, double alpha_prior,
                                      double[::1] mu_rates,
                                      double[::1] beta_rates,
-                                     map[int, map[int, int]] &search_lower,
                                      FPTree fptree) nogil:
 
     cdef double mu_rate = mu_rates[proc_a]
-    cdef double ti = all_timestamps[proc_a][i]
-    cdef double tp = prev_back_t
-    cdef double dt = ti - tp
-    cdef double mu_prob = mu_rate * dt * exp(-mu_rate*dt)
+    cdef double mu_prob = mu_rate * dt_background * exp(-mu_rate*dt_background)
 
     cdef int candidate = fptree.sample(rand())
     cdef double q_c = fptree.get_value(candidate)
     cdef double a_ca = 0
     cdef double p_c = 0
-    busca_probability(i, candidate, proc_a, all_timestamps, Alpha_ab, sum_b,
-                      alpha_prior, beta_rates, lower_stamps, &a_ca, &p_c)
+    busca_probability(candidate, proc_a, Alpha_ab, sum_b, alpha_prior,
+                      beta_rates, t, last_t[candidate], &a_ca, &p_c)
 
     cdef double q_b = 0
     cdef double a_ba = 0
     cdef double p_b = 0
     if curr_influencer_b != -1:
         q_b = fptree.get_value(curr_influencer_b)
-        busca_probability(i, candidate_j, proc_a, all_timestamps, Alpha_ab,
-                          sum_b, alpha_prior, beta_rates, lower_stamps,
-                          &a_ba, &p_b)
+        busca_probability(curr_influencer_b, proc_a, Alpha_ab, sum_b,
+                          alpha_prior, beta_rates, t,
+                          last_t[curr_influencer_b], &a_ba, &p_b)
 
     cdef double busca_rate_c = p_c / a_ca
-    cdef int n_proc = <int>all_timestamps.size()
     if sample_background(mu_prob / (mu_prob + busca_rate_c)):
         return -1
     elif curr_influencer_b == -1:
@@ -127,31 +119,24 @@ cdef inline int metropolis_walk_step(int proc_a, int i, int curr_influencer_b,
             return curr_influencer_b
 
 
-cdef void sample_alpha(int i, double[::1] timestamps, int[::1] process_id,
-                       int[::1] curr_state, int[::1] num_background,
-                       double dt_background,
-                       map[int, map[int, int]] &Alpha_ab, int[::1] sum_b,
-                       double[::1] mu_rates, double[::1] beta_rates,
-                       double alpha_prior, FPTree fptree,
-                       map[int, map[int, int]] &search_lower) nogil:
+cdef int sample_alpha(int proc_a, double t, int influencer,
+                      double[::1] last_t, int[::1] num_background,
+                      double dt_background,
+                      map[int, map[int, int]] &Alpha_ab, int[::1] sum_b,
+                      double[::1] mu_rates, double[::1] beta_rates,
+                      double alpha_prior, FPTree fptree) nogil:
 
-    cdef int influencer = curr_state[i]
     cdef int new_influencer
-    cdef double prev_back_t = 0      # stores last known background time stamp
-    cdef double prev_back_t_aux = 0  # every it: prev_back_t = prev_back_t_aux
-
     if influencer == -1:
         num_background[proc_a] -= 1
-        prev_back_t_aux = all_timestamps[proc_a][i] # found a background ts
     else:
         Alpha_ab[proc_a][influencer] -= 1
         sum_b[influencer] -= 1
 
-        new_influencer = metropolis_walk_step(proc_a, i, dt_background,
-                                              influencer, num_background,
-                                              all_timestamps, curr_state,
-                                              Alpha_ab, sum_b, alpha_prior,
-                                              mu_rates, beta_rates, fptree)
+    new_influencer = metropolis_walk_step(proc_a, influencer, t, last_t,
+                                          dt_background, Alpha_ab, sum_b,
+                                          alpha_prior, mu_rates, beta_rates,
+                                          fptree)
     if new_influencer == -1:
         num_background[proc_a] += 1
     else:
@@ -164,8 +149,7 @@ cdef void sample_alpha(int i, double[::1] timestamps, int[::1] process_id,
 
     if influencer != -1:
         fptree.set_value(influencer, fptree.get_value(influencer) - 1)
-        curr_state[i] = new_influencer
-    prev_back_t = prev_back_t_aux
+    return new_influencer
 
 
 cdef void update_mu_rate(double[::1] last_t_background,
@@ -191,7 +175,7 @@ cdef void update_beta_rate(double[::1] timestamps, int[::1] process_id,
     cdef double t, dt
 
     for proc_a in range(last_t.shape[0]):
-        last_t[proc_a] = -1
+        last_t[proc_a] = 0
 
     for i in range(timestamps.shape[0]):
         t = timestamps[i]
@@ -200,12 +184,7 @@ cdef void update_beta_rate(double[::1] timestamps, int[::1] process_id,
         if proc_b == -1:
             continue
 
-        dt = 0
-        if last_t[proc_b] == -1:
-            dt = t
-        else:
-            dt = t - last_t[proc_b]
-
+        dt = t - last_t[proc_b]
         if all_deltas.count(proc_a) == 0:
             all_deltas[proc_a] = vector[double]()
         all_deltas[proc_a].push_back(dt)
@@ -217,14 +196,15 @@ cdef void update_beta_rate(double[::1] timestamps, int[::1] process_id,
         if n >= 1:
             stdsort(all_deltas[proc_a].begin(), all_deltas[proc_a].end())
             beta_rates[proc_a] = all_deltas[proc_a][n // 2]
-            if n_elements % 2 == 0:
+            if n % 2 == 0:
                 beta_rates[proc_a] += all_deltas[proc_a][(n // 2)-1]
                 beta_rates[proc_a] = beta_rates[proc_a] / 2
         else:
-            beta_rates[proc_a] = timestamps[timestamps.shape[0] - 1]
+            beta_rates[proc_a] = last_t[proc_a]
 
 
 cdef void sampleone(double[::1] timestamps, int[::1] process_id,
+                    double[::1] last_t, double[::1] last_t_background,
                     int[::1] curr_state, int[::1] num_background,
                     double[::1] mu_rates, double[::1] beta_rates,
                     map[int, map[int, int]] &Alpha_ab, double alpha_prior,
@@ -240,13 +220,20 @@ cdef void sampleone(double[::1] timestamps, int[::1] process_id,
                      sum_b, beta_rates)
 
     printf("[logger]\t Sampling Alpha.\n")
-    cdef int i, a, b
+    cdef int i, a, b, new_b
+    cdef double t
+
+    for a in range(last_t.shape[0]):
+        last_t[a] = 0
+        last_t_background[a] = 0
 
     for i in range(timestamps.shape[0]):
+        t = timestamps[i]
         a = process_id[i]
-        sample_alpha(t, a, curr_state, num_background, Alpha_ab, sum_b,
-                     mu_rates, beta_rates, alpha_prior, fptrees[a],
-                     last_t, last_t_background[a])
+        new_b = sample_alpha(a, t, curr_state[i], num_background,
+                             t - last_t_background[a], Alpha_ab, sum_b,
+                             mu_rates, beta_rates, alpha_prior, fptrees[a])
+        curr_state[i] = new_b
         if curr_state[i] == -1:
             last_t_background[i] = t
         last_t[a] = t
@@ -298,7 +285,7 @@ def fit(double[::1] timestamps, int[::1] process_id, int n_proc,
     cdef int i, a, b
     for i in range(n_stamps):
         a = process_id[i]
-        b = np.random.randint(-1, n_proc, len(all_timestamps[a]))
+        b = np.random.randint(-1, n_proc)
         curr_state[i] = b
         if Alpha_ab.count(a) == 0:
             Alpha_ab[a] = map[int, int]()
