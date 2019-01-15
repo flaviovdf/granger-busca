acf65267315794a0b16439550c90eb006c648038
Wisemap <guilherme.resende.borges@gmail.com>
2019-01-15 13:06:23 -0200

    simulating through time change

--- gb/simulate.pyx
+++ gb/simulate.pyx
@@ -57,32 +57,6 @@ class GrangeBuscaSimulator(object):
         self.thinning = thinning
         self.t = 0
 
-    def total_intensity(self, t):
-        lambdas_t = np.zeros(self.mu_rates.shape[0], dtype='d')
-        for proc_a in range(self.n_nodes):
-            lambdas_t[proc_a] = self.mu_rates[proc_a]
-            if len(self.past[proc_a]) == 0:
-                continue
-
-            tp = self.past[proc_a][-1]
-            assert tp <= t
-            for proc_b in range(self.n_nodes):
-                if len(self.past[proc_b]) == 0:
-                    continue
-
-                tpp_idx = bisect(self.past[proc_b], tp)
-                if tpp_idx == len(self.past[proc_b]):
-                    tpp_idx -= 1
-                tpp = self.past[proc_b][tpp_idx]
-                while tpp >= tp and tpp_idx > 0:
-                    tpp_idx -= 1
-                    tpp = self.past[proc_b][tpp_idx]
-                if tpp >= tp:
-                    continue
-                busca_rate = self.Alpha_ba[proc_b, proc_a]
-                busca_rate /= (self.Beta_ba[proc_b, proc_a] + tp - tpp)
-                lambdas_t[proc_a] += busca_rate
-        return lambdas_t
 
     def simulate(self, forward):
         cdef float t = self.t        
@@ -92,18 +66,13 @@ class GrangeBuscaSimulator(object):
         while t < max_time:
             lambdas_t = total_intensity2(t, self.Alpha_ba, self.Beta_ba, self.mu_rates, self.past)
             sum_lambdas_t = lambdas_t.cumsum()
-            if self.thinning:
-                dt = np.random.exponential(1.0 / self.upper_bound)
-            else:
-                dt = np.random.exponential(1.0 / sum_lambdas_t[-1])
+ 
+            dt = np.random.exponential(1.0)
 
+            dt = dt/sum_lambdas_t[-1]
             t = t + dt
             if t > max_time:
                 break
-
-            if self.thinning:
-                if np.random.rand() < (sum_lambdas_t[-1] / self.upper_bound):
-                    continue
 
             i = 0
             u = np.random.rand() * sum_lambdas_t[-1]
